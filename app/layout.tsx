277ade1bb27a8732d59ccfc742d1c7c7c7a2b7ff
athul suresh <athulrsuresh@gmail.com>
2025-09-03 16:02:07 -0500

    modified laout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Attendance Form",
-  description: "A simple and elegant attendance form.",
+  title: "SAE Attendance Form",
+  description: "The SAE Attendence Form.",
 };
 
 export default function RootLayout({
